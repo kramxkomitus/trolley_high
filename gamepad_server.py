da2b8621c0dc7c9e87d00d0b9e8157020143c126
mark <kramxkomitus@gmail.com>
2022-12-14 17:08:06 +0400

    14.12.22

--- gamepad_server.py
+++ gamepad_server.py
@@ -2,7 +2,36 @@ import pygame
 import serial
 import time 
 import subprocess 
-from math import sqrt
+
+L_vel = 0
+R_vel = 0
+
+def trig_func(axis):
+    
+    axis = int(500 * (axis + 1))
+    if axis < 1:
+        return 0
+    else:
+        return axis
+
+def set_vel(R, L):
+    global L_vel, R_vel
+    if R == 0:
+        R_vel = 0
+    elif R > R_vel >= 0 :
+        R_vel += 1
+    elif R_vel < R:
+        R_vel -= 1
+
+    if L == 0:
+        L_vel = 0
+    elif L > L_vel >= 0 :
+        L_vel += 1
+    elif L_vel < L:
+        L_vel -= 1
+    return
+
+
 
 
 controller_MAC = "C8:3F:26:B8:00:16"
@@ -14,18 +43,13 @@ while "Connection successful" not in output:
     print(output)
 
 print("finding TTL:")
-i = 0
-while True:
-    serial_path = "/dev/ttyUSB" + str(i)
-    HW = serial.Serial(serial_path, baudrate=115200)
-    if HW.isOpen() == True:
-        print("found" + serial_path)
-        break
+serial_path = "/dev/ttyUSB1"
+HW = serial.Serial(serial_path, baudrate=115200)
+if HW.isOpen() == True:
+    print("found" + serial_path)
+else:
     print("can't reach " + serial_path)
-    
-
 pygame.init()
-pygame.display.init()
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
@@ -40,14 +64,9 @@ while True:
     X = joystick.get_button(2)
     Y = joystick.get_button(0)
 
-    left_inv, right_inv = joystick.get_button(6), joystick.get_button(7)
-    left, right = joystick.get_axis(5), joystick.get_axis(4)
-    
-    # left = (485 * (left + 1) + 1)
-    # right = (485  * (right + 1) + 1)
-    # left = int(1000 - 1000 / sqrt(left))
-    # right = int(1000 - 1000 / sqrt(right))
-    
+    left_inv, right_inv = joystick.get_button(4), joystick.get_button(5)
+    left, right = joystick.get_axis(5), joystick.get_axis(2)
+
     if left_inv:
         left = -left
     if right_inv:
@@ -55,47 +74,38 @@ while True:
 
     if A:
         HW.write(f"start\n".encode())
+        A = 0
+        print("start")
     if B:
-        HW.writable(f"stop\n".encode())
+        HW.write(f"stop\n".encode())
+        B = 0
+        print("stop")
 
-    print(f"{left:.2f}" + "\t\t\t" +f"{right:.2f}")
+    left = trig_func(left)
+    right = trig_func(right)
 
-    # print(f"{left}" + "\t\t\t" + f"{right}")
-    # HW.write(f"R {right}\n".encode())
-    # time.sleep(0.1)
-    # HW.write(f"L {left}\n".encode())
+    set_vel(left, right)
+
+    R_str = "R " + str(R_vel)
+    L_str = "L " + str(L_vel)
+    print(R_str + "       " + L_str + '\r')
+    HW.write((R_str + '\n').encode())
+    HW.write((L_str + '\n').encode())
+    time.sleep(0.1)
+    
     
 
 
 
 
+while True:
+    serial_path = "/dev/ttyUSB1"
+    #  + str(i)
+    HW = serial.Serial(serial_path, baudrate=115200)
+    if HW.isOpen() == True:
+        print("found" + serial_path)
+        break
+    print("can't reach " + serial_path)
+    
 
-
-# # Main loop
-# while True:
-#     pygame.event.pump()
-#     rt, lt = joystick. get_axis(0), joystick.get_axis(3)
-#     rt = max(0, min(rt, 1))
-#     lt = max(0, min(lt, 1))
-
-#     start, stop = joystick.get_button(0), joystick.get_button(1)
-
-#     # Send the joystick position to the WebSocket
-#     data = f"{rt},{lt}"
-#     # ,{start},{stop}"
-#     print(data)
-#     # s.sendall(data.encode())
-
-#     # Wait a bit before sending the next position
-#     time.sleep(0.1)
-
-
-
-
-# # Set up the Wi-Fi connection
-# HOST = '127.0.0.1'  # IP address of the PC
-# PORT = 8888
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen()
-# conn, addr = s.accept()
+pygame.init()
